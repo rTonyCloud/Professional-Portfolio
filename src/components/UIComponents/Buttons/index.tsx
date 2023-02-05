c14176a8338463e88e5102b52c31d16934296e14
rtonycloud <rtonycloud@gmail.com>
2023-02-04 23:27:11 -0500

    initial

---
+++ src/components/UIComponents/Buttons/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+interface Props {
+    children: React.ReactNode
+    variant?: 'blue' | 'gray' | 'white'
+    disabled?: boolean
+    sx?: React.CSSProperties
+    type?: 'button' | 'submit' | 'reset' | undefined
+    onClick?: () => void
+}
+
+const Button: React.FC<Props> = (props: Props): JSX.Element => {
+    const { children, variant, sx, onClick, disabled, type } = props
+
+    const returnButtonBg = (): string => {
+        switch (variant) {
+            case 'gray':
+                return '#5D5B69'
+            case 'white':
+                return '#ffffff'
+            default:
+                return '#007CF0'
+        }
+    }
+
+    return <button
+    onClick={onClick}
+    type={type}
+    disabled={disabled}
+    style={{
+      fontSize: '18px',
+      fontWeight: 500,
+      fontFamily: 'Montserrat',
+      minWidth: '220px',
+      width: 'max-content',
+      borderRadius: '24px',
+      cursor: 'pointer',
+      padding: '8px 12px',
+      backgroundColor: returnButtonBg(),
+      color: '#fff',
+      ...sx,
+    }}
+  >
+    {children}
+  </button>
+}
+
+export default Button
