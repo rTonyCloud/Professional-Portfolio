ac6790110d1b206f7c1605e686155b6e3076d6b0
rtonycloud <rtonycloud@gmail.com>
2023-02-05 11:39:49 -0500

    added structure in assets and babel implements with style components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    compiler: {
+        //enables swc for style components
+        styledComponents: true,
+    },
+    reactStrictMode: true,
 }
 
 module.exports = nextConfig
