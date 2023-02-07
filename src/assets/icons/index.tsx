30b5aa2524da64b2a4d0d58bf6f407c241e6f674
rtonycloud <rtonycloud@gmail.com>
2023-02-06 19:23:54 -0500

    icon exported

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,1 +1,50 @@
-export {}
+import blockchainIcon from './blockchainIcon.svg';
+import bracketCodeIcon from './bracketCodeIcon.svg';
+import closeCodeIcon from './closeCodeIcon.svg';
+import cssIcon from './cssIcon.svg';
+import dancingManIcon from 'dancingManIcon.svg';
+import darkCircleIcon from './darkCircleIcon.svg';
+import fiveStarIcons from './fiveStarIcons.svg';
+import footerGithubIcon from './footerGithubIcon.svg';
+import footerGmailIcon from './footerGmailIcon.svg';
+import footerLinkedinIcon from './footerLinkedinIcon.svg';
+import githubIcon from './githubIcon.svg';
+import heartIcon from './heartIcon.svg';
+import html5Icon from './html5Icon.svg';
+import javascriptIcon from './javascriptIcon.svg';
+import linkedinIcon from './linkedinIcon.svg';
+import nextjsIcon from './nextjsIcon.svg';
+import nodejsIcon from './nodejsIcon.svg';
+import profileCircleIcon from './profileCircleIcon.svg';
+import reactIcon from './reactIcon.svg';
+import reduxIcon from './reduxIcon.svg';
+import rightArrowIcon from './rightArrowIcon.svg';
+import typescriptIcon from './typescriptIcon.svg';
+import rtonycloudLogo from './rtonycloudLogo.svg';
+
+
+export {
+    blockchainIcon,
+    bracketCodeIcon,
+    closeCodeIcon,
+    cssIcon,
+    dancingManIcon,
+    darkCircleIcon,
+    fiveStarIcons,
+    footerGithubIcon,
+    footerGmailIcon,
+    footerLinkedinIcon,
+    githubIcon,
+    heartIcon,
+    html5Icon,
+    javascriptIcon,
+    linkedinIcon,
+    nextjsIcon,
+    nodejsIcon,
+    profileCircleIcon,
+    reactIcon,
+    reduxIcon,
+    rightArrowIcon,
+    typescriptIcon,
+    rtonycloudLogo,
+}
