518a0ea2e7347a25fa4e179c140cd64ab0375b13
rtonycloud <rtonycloud@gmail.com>
2023-02-19 20:05:59 -0500

    fix error

--- src/utils/portfolioTexts/footer.texts.tsx
+++ src/utils/portfolioTexts/footer.texts.tsx
@@ -8,25 +8,36 @@ import {
 const footerCopyRight =
     'All rights reserved © 2023 - SOFTWARE DEVELOPER - by rTONYCLOUD'
 
-// const footerLinks = [
-//     {
-//         image: <Image src={footerGmailIcon} alt="Gmail" height={50} width={50} />,
-//         title: 'Gmail',
-//         url: 'mailto:',
-//         alt: 'Gmail',
-//     },
-//     {
-//         image: <Image src={footerLinkedinIcon} alt="Linkedin" height={50} width={50} />,
-//         title: 'Linkedin',
-//         url: 'www.linkedin.com/in/tony-rivera-full-stack-engineer',
-//         alt: 'Linkedin',
-//     },
-//     {
-//         image: <Image src={footerGithubIcon} alt="Github" height={50} width={50} />,
-//         alt: 'Github',
-//         title: 'Github',
-//         url: 'www.github.com/rtonycloud',
-//     },
-// ]
+const footerLinks = [
+    {
+        image: (
+            <Image src={footerGmailIcon} alt="Gmail" height={50} width={50} />
+        ),
+        title: 'Gmail',
+        url: 'mailto:',
+        alt: 'Gmail',
+    },
+    {
+        image: (
+            <Image
+                src={footerLinkedinIcon}
+                alt="Linkedin"
+                height={50}
+                width={50}
+            />
+        ),
+        title: 'Linkedin',
+        url: 'www.linkedin.com/in/tony-rivera-full-stack-engineer',
+        alt: 'Linkedin',
+    },
+    {
+        image: (
+            <Image src={footerGithubIcon} alt="Github" height={50} width={50} />
+        ),
+        alt: 'Github',
+        title: 'Github',
+        url: 'www.github.com/rtonycloud',
+    },
+]
 
-export { footerCopyRight }
+export { footerCopyRight, footerLinks }
