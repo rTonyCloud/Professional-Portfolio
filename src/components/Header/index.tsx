d43fec1647058782e40a7298fb33f02c28215996
rtonycloud <rtonycloud@gmail.com>
2023-02-19 22:48:08 -0500

    update texts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,33 @@ import Image from 'next/image'
 import * as styles from './header.styles'
 import stylesGuide from '@/styles/components/components.styles'
 import { Button } from '../UIComponents/'
-//@ts-ignore
-import {
-    NavItems,
-    MediaItems,
-    ButtonText,
-} from '../../utils/portfolioTexts/Header.texts'
-//@ts-ignore
+import { githubIcon, linkedinIcon, heartIcon } from 'public/icons'
+
 const { HeaderbuttonStyles } = stylesGuide()
+
+const NavItems = ['About', 'Skills', 'Projects']
+
+const MediaItems = [
+    {
+        image: (
+            <Image src={githubIcon} alt="github" width="30" className="image" />
+        ),
+        title: 'Github',
+        url: 'www.github.com/rtonycloud',
+    },
+    {
+        image: <Image src={linkedinIcon} alt="Linkedin" width="30" />,
+        title: 'LinkedIn',
+        url: 'www.linkedin.com/in/tony-rivera-full-stack-engineer',
+    },
+]
+
+const ButtonText = [
+    {
+        image: <Image src={heartIcon} alt="hello" width="20" />,
+        text: 'Download Resume',
+    },
+]
 
 const Header: React.FC = (): JSX.Element => {
     return (
